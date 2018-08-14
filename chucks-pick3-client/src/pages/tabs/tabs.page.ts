59a27ff62bb662354ba4bc70cdac76c79283f17b
K. Alex Mills <k.alex.mills@gmail.com>
2018-08-14 15:22:37 -0500

    Tabs pack similarly modularized

--- chucks-pick3-client/src/pages/tabs/tabs.page.ts
+++ chucks-pick3-client/src/pages/tabs/tabs.page.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import {IonicPage} from "ionic-angular";
 
 import { HistoryPage } from '../history/history.page';
 import { ContactPage } from '../contact/contact.page';
 import { TodayPage } from '../today/today.page';
 
+@IonicPage()
 @Component({
   templateUrl: 'tabs.html'
 })
