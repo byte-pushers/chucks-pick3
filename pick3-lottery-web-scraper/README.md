d142193619dee0ed8c69955598ce38686032b0b6
K. Alex Mills <k.alex.mills@gmail.com>
2018-08-17 14:18:56 -0500

    Remove lottery web scraper from this project. See npm project.

--- pick3-lottery-web-scraper/README.md
+++
@@ -1,2 +0,0 @@
-# pick3-lottery-web-scraper
-Web scraper that gets past winning numbers for the Pick 3 lottery.
--- pick3-lottery-web-scraper/src/main/javascript/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { WebScraper } from "./software.bytepushers.pick3.lottery.web.WebScraper";
-
-export interface BytePushers {
-    WebScraper(): WebScraper;
-}
