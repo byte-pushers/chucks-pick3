e80ff271915a1e47db5296cf1e849475f1e85f88
K. Alex Mills <k.alex.mills@gmail.com>
2018-08-29 13:48:24 -0500

    Date / Time select page now looks like the mockup.

--- chucks-pick3-client/src/app/pages/future-select/future-select.page.module.ts
+++ chucks-pick3-client/src/app/pages/future-select/future-select.page.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import {IonicPage, IonicPageModule} from 'ionic-angular';
 import { FutureSelectPage } from './future-select.page';
+import {TitleCasePipe} from "@angular/common";
 
 @IonicPage()
 @NgModule({
-  declarations: [FutureSelectPage],
+  declarations: [ FutureSelectPage ],
+  providers: [ TitleCasePipe ],
   imports: [
     IonicPageModule.forChild(FutureSelectPage)
   ]
