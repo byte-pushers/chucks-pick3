d668fef3c65ba458d152fe54486ef8fe4c49a052
K. Alex Mills <k.alex.mills@gmail.com>
2018-09-07 09:16:03 -0500

    fix: leftpad the winning number on "Get New Picks" page.

--- chucks-pick3-client/src/app/pages/future-select/future-select.page.module.ts
+++ chucks-pick3-client/src/app/pages/future-select/future-select.page.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import {IonicPage, IonicPageModule} from 'ionic-angular';
 import { FutureSelectPage } from './future-select.page';
 import {TitleCasePipe} from "@angular/common";
+import {PipesModule} from "../../pipes/pipes.module";
 
 @IonicPage()
 @NgModule({
   declarations: [ FutureSelectPage ],
   providers: [ TitleCasePipe ],
   imports: [
+    PipesModule,
     IonicPageModule.forChild(FutureSelectPage)
   ]
 })
