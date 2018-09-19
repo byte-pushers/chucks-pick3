7bb4fd0808506bf77b44437c61e2ddd88e3cbd65
K. Alex Mills <k.alex.mills@gmail.com>
2018-09-19 09:16:35 -0500

    Update history.page.ts
    Unneeded comments clutter the codebase. 👍

--- chucks-pick3-client/src/app/pages/history/history.page.ts
+++ chucks-pick3-client/src/app/pages/history/history.page.ts
@@ -48,7 +48,6 @@ export class HistoryPage {
 
   private parseDate(d: string): Date {
     let parts: any = d.match(/(\d+)/g);
-    // new Date(year, month [, date [, hours[, minutes[, seconds[, ms]]]]])
     return new Date(parts[0], parts[1] - 1, parts[2]); // months are 0-based
   }
 }
