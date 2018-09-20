f38f3740d2659c04e37bd5824f35812c5ab6a1b5
K. Alex Mills <k.alex.mills@gmail.com>
2018-09-20 13:44:30 -0500

    chore: document karma conf, as devs may change values locally

--- chucks-pick3-client/karma.conf.js
+++ chucks-pick3-client/karma.conf.js
@@ -45,9 +45,9 @@ module.exports = function (config) {
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['ChromeHeadless'],
-    singleRun: true,
+    autoWatch: false,
+    browsers: ['ChromeHeadless'], /// ChromeHeadless in CI
+    singleRun: true,              /// true in CI
     junitReporter: {
       outputDir: process.env.JUNIT_REPORT_PATH,
       outputFile: process.env.JUNIT_REPORT_NAME,
