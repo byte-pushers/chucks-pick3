a0772fa55e85a2cca0ea2638d324f158cc24ebc9
K. Alex Mills <k.alex.mills@gmail.com>
2018-09-24 10:34:07 -0500

    test: add test for draw results.

--- chucks-pick3-client/e2e/today.e2e-spec.ts
+++ chucks-pick3-client/e2e/today.e2e-spec.ts
@@ -5,7 +5,7 @@ import { browser, element, by } from 'protractor';
 describe('Today Tab', () => {
 
   beforeEach(async () => {
-    await browser.get('#/tab/today/results');
+    await browser.get('#/tab/today/results/2008-09-19') // using a past date to ensure determinism
   });
 
   it('should have a title', () => {
@@ -16,13 +16,25 @@ describe('Today Tab', () => {
     expect(element(by.css('ion-tab[tabtitle="Today"].show-tab')).isPresent()).toEqual(true);
   });
 
-  it('should show progress bars immediately', () => {
+  it('should show progress bars', () => {
     expect(element(by.tagName('progress-indeterminate')).isPresent()).toEqual(true);
   });
 
-  it('should complete all web scrapes within 5s of page load', () => {
+  it('should complete all scrapes within 5s of page load', () => {
     setTimeout(() => {
       expect(element(by.tagName('progress-indeterminate')).isPresent()).toEqual(false);
     }, 5000);
   });
+
+  it('should correctly report the draw results for 2008-09-19', () => {
+    setTimeout(() => {
+      let expected = ['N/A', '468', 'N/A', '481'],
+          els = element.all(by.className('win-number'));
+      for (let i = 0; i < expected.length; ++i) {
+        expect(els.get(i).getText()).toEqual(expected[i]);
+        console.log(els.get(i).getText());
+      }
+    }, 5000);
+  });
+
 });
