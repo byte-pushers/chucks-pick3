de67b8ae0899e310d70d1e85cecf231f1456f739
K. Alex Mills <k.alex.mills@gmail.com>
2018-09-24 12:56:46 -0500

    test: future-select e2e tests

--- chucks-pick3-client/e2e/future-select.e2e-spec.ts
+++ chucks-pick3-client/e2e/future-select.e2e-spec.ts
@@ -16,22 +16,38 @@ describe('Today Tab', () => {
     expect(element(by.css('page-future-select')).isDisplayed()).toEqual(true, 'today tab not shown');
   });
 
-  it('should show date picker when "select-day" widget is clicked', () => {
-    element(by.id('select-day')).click();
-    browser.driver.sleep(300);
-    expect(element(by.tagName('ion-picker-cmp')).isPresent()).toEqual(true);
+  it('should allow the user to submit after selecting both date and time', () => {
+    selectDate();
+    selectTime();
+    expect(element(by.cssContainingText('button', 'Submit')).isEnabled()).toEqual(true, "submit button not enabled after selecting date and time");
+
+    element(by.cssContainingText('button', 'Submit')).click();
+
+    browser.driver.sleep(2000);
+    expect(browser.getCurrentUrl()).toMatch(/.*numbers\/2008-09-19\/DAY\/468\/\d{4}-\d{2}-\d{2}\/NIGHT/, 'URL params not correct');
   });
 
-  it('should allow the user to select the time', () => {
+  function selectDate() {
+    element(by.id('select-day')).click();
+    browser.driver.sleep(300);
+    expect(element(by.tagName('ion-picker-cmp')).isPresent()).toEqual(true, 'date picker not displayed 300ms after clicking "select-day" widget');
+
+    element(by.buttonText('Done')).click();
+    browser.driver.sleep(1000);
+    expect(element(by.tagName('ion-picker-cmp')).isPresent()).toEqual(false, 'date picker still present 1s after clicking Done button');
+    expect(element(by.id('select-day')).$('ion-icon').getAttribute('ng-reflect-color')).toEqual('primary', 'date color not changed after selection');
+  }
+
+  function selectTime() {
     element(by.id('select-time')).click();
     browser.driver.sleep(300);
-    expect(element(by.tagName('ion-action-sheet')).isPresent()).toEqual(true);
+    expect(element(by.tagName('ion-action-sheet')).isPresent()).toEqual(true, 'action sheet not present 300ms after clicking "select-time" widget');
     expect(element(by.className('action-sheet-title')).getText()).toEqual('Select time of drawing');
 
-    element(by.css)
     element(by.cssContainingText('button', 'Night')).click();
     browser.driver.sleep(1000);
     expect(element(by.tagName('ion-action-sheet')).isPresent()).toEqual(false);
     expect(element(by.id('select-time')).$('ion-label').getText()).toEqual('Night');
-  });
+  }
+
 });
