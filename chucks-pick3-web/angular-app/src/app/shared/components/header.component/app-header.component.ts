4e337464bb871c86ae8c531fd4ce3d8cc333bb19
David Ocampo <david.ocampo@bytepushers.software>
2019-07-15 12:54:13 -0500

    Completed sub nav for angular.

--- chucks-pick3-web/angular-app/src/app/shared/components/header.component/app-header.component.ts
+++ chucks-pick3-web/angular-app/src/app/shared/components/header.component/app-header.component.ts
@@ -6,10 +6,27 @@ import {Component, OnInit } from '@angular/core';
   styleUrls: ['./app-header.component.css']
 })
 
-export class AppHeaderComponent implements OnInit{
-  constructor() {}
+export class AppHeaderComponent implements OnInit {
+  constructor() {
+  }
+
   ngOnInit() {
   }
 
-
+  public showSubNav() {
+    const nav = document.getElementById('showNav');
+    if (nav.style.display === 'none') {
+      nav.style.display = 'block';
+    } else {
+      nav.style.display = 'block';
+    }
+  }
+  public hideSubNav() {
+    const nav = document.getElementById('showNav');
+    if (nav.style.display === 'block') {
+      nav.style.display = 'none';
+    } else {
+      nav.style.display = 'none';
+    }
+  }
 }
