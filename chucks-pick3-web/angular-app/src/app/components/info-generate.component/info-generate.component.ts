d9c1e70e9c00d65dc90c004a8adc129b45575fa9
David Ocampo <david.ocampo@bytepushers.software>
2019-07-16 10:33:46 -0500

    Completed routing for sub nav, allowed for divs for each button to be fully clickable.

---
+++ chucks-pick3-web/angular-app/src/app/components/info-generate.component/info-generate.component.ts
@@ -0,0 +1,18 @@
+import {Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-info-generate',
+  templateUrl: './info-generate.component.html',
+  styleUrls: ['./info-generate.component.css']
+})
+
+export class InfoGenerateComponent implements OnInit {
+  constructor() {
+  }
+
+  ngOnInit() {
+
+  }
+
+}
+
---
+++ chucks-pick3-web/angular-app/src/app/components/info-interface.component/info-interface.component.ts
@@ -0,0 +1,17 @@
+import {Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-info-interface',
+  templateUrl: './info-interface.component.html',
+  styleUrls: ['./info-interface.component.css']
+})
+
+export class InfoInterfaceComponent implements OnInit {
+  constructor() {
+  }
+
+  ngOnInit() {
+
+  }
+}
+
