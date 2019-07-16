6cad805cc61d84fbbca7e50cd1dd437553339b7f
David Ocampo <david.ocampo@bytepushers.software>
2019-07-16 13:03:22 -0500

    Completed alignment within info page.

---
+++ chucks-pick3-web/angular-app/src/app/components/info-view.component/info-view.component.ts
@@ -0,0 +1,17 @@
+import {Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-info-view',
+  templateUrl: './info-view.component.html',
+  styleUrls: ['./info-view.component.css']
+})
+
+export class InfoViewComponent implements OnInit {
+  constructor() {
+  }
+
+  ngOnInit() {
+
+  }
+}
+
