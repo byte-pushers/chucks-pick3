84af42548159f2b40a97b15b194f5daa43f7a033
David Ocampo <david.ocampo@bytepushers.software>
2019-07-16 18:01:31 -0500

    Aligned content in splash page.

---
+++ chucks-pick3-web/angular-app/src/app/shared/components/app-main.component/app-main.component.ts
@@ -0,0 +1,15 @@
+import {Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './app-main.component.html',
+  styleUrls: ['./app-main.component.css']
+})
+
+export class AppMainComponent implements OnInit {
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+}
