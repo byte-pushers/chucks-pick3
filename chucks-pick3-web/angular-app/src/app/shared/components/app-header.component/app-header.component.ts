d05fba3fe3c606ac80ab1e477757ad424f77b86a
David Ocampo <david.ocampo@bytepushers.software>
2019-07-18 16:44:45 -0500

    Worked on changes to subnav, added closing function to links in sidebar, currently fixing containers to link properly into angular.

--- chucks-pick3-web/angular-app/src/app/shared/components/app-header.component/app-header.component.ts
+++ chucks-pick3-web/angular-app/src/app/shared/components/app-header.component/app-header.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,15 +7,15 @@ import {Component, OnInit } from '@angular/core';
 })
 
 export class AppHeaderComponent implements OnInit {
+  public showSubNavBarStatus = false;
+
   constructor() {
   }
-  public showNav = false;
-
   ngOnInit() {
   }
 
-  public showSubNav(showStatus: boolean): void {
-    this.showNav = showStatus;
+  public showSubNavBar(showSubNavBarStatus: boolean): void {
+    this.showSubNavBarStatus = showSubNavBarStatus;
   }
 
   public openNav() {
