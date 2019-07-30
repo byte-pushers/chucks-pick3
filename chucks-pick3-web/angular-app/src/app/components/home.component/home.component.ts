cb0c51de26712e16eef97579c4a448cfb47404cd
Tonté Pouncil <pouncilt.developer@gmail.com>
2019-07-30 15:31:29 -0500

    WIP - Working on a fix.

--- chucks-pick3-web/angular-app/src/app/components/home.component/home.component.ts
+++ chucks-pick3-web/angular-app/src/app/components/home.component/home.component.ts
@@ -1,4 +1,7 @@
 import {Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {SubNavBarService} from "../../services/show-sub-nav-bar.service/sub-nav-bar.service";
+import {map} from "rxjs/operators";
 
 @Component({
   selector: 'app-home',
@@ -8,10 +11,17 @@ import {Component, OnInit } from '@angular/core';
 
 // tslint:disable-next-line:component-class-suffix
 export class HomeComponent implements OnInit {
-  constructor() {
+  constructor(private subNavBarService: SubNavBarService, private route: ActivatedRoute) {
   }
 
   ngOnInit() {
-
+    this.route.queryParamMap.pipe(map(params => {
+      // TODO: before using params, check to make sure it is not null or undefined.
+      // TODO: After calling params.get('showSubNavBar') set return value to variable and check to make sure it is not null or undefined.
+      // TODO: HINT: You only can call toLowerCase() method on a string.
+      return params.get('showSubNavBar').toLowerCase() === 'true' ? true : false;
+    })).subscribe(subNavBarVisible => {
+      this.subNavBarService.setSubNavBarVisibility(subNavBarVisible);
+    });
   }
 }
