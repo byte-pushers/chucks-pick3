3e35c3dd9ccf65619120e8bf3c5e0a0d2cdeb77d
David Ocampo <david.ocampo@bytepushers.software>
2019-09-04 17:25:46 -0500

    Created random customer generator.

--- chucks-pick3-web/angular-app/src/app/components/customer-summary.component/customer-summary.component.ts
+++ chucks-pick3-web/angular-app/src/app/components/customer-summary.component/customer-summary.component.ts
@@ -49,13 +49,13 @@ export class CustomerSummaryComponent implements OnInit {
     const customer = this.customers;
     // TODO: create a new customer.
     const newCust = {
-      firstName: this.generateCustomer(),
-      lastName: 'Bowman',
-      email: 'jbowman@gmail.com',
+      firstName: this.generateFirstName(),
+      lastName: this.generateLastName(),
+      email: this.generateEmail(),
       phone: '555-555-5555',
-      city: 'Chicago',
-      state: 'Illinois',
-      id: 76
+      city: this.generateCity(),
+      state: this.generateState(),
+      id: this.generateId()
     };
     // TODO: const aCustomer = this.generateCustomer()
     // TODO: add new customer to array.
@@ -63,12 +63,63 @@ export class CustomerSummaryComponent implements OnInit {
     /*   });*/
   }
 
-  private generateCustomer() {
+  private generateFirstName() {
     const firstName =
       [
-        'Harvey', 'Murray', 'Jack'
+        'Harvey',
+        'Murray',
+        'Jack'
       ];
-    return firstName[Math.floor(Math.random() * 3)];
+    return firstName[Math.floor(Math.random() * 4)];
+  }
+
+  private generateLastName() {
+    const lastName =
+      [
+        'Pliskin',
+        'Smith',
+        'Gallegos'
+      ];
+    return lastName[Math.floor(Math.random() * 4)];
+  }
+
+  private generateEmail() {
+    const email =
+      // these are temps
+      [
+        'baseballstar@gmail.com',
+        'rockets@gmail.com',
+        'supernintendo@gmail.com'
+      ];
+    // TODO find way to combine first and last name in order to create email.
+    return email[Math.floor(Math.random() * 4)];
+  }
+  private generateCity() {
+    const lastName =
+      [
+        'Tampa',
+        'Los Angeles',
+        'Portland'
+      ];
+    return lastName[Math.floor(Math.random() * 4)];
+  }
+  private generateState() {
+    const lastName =
+      [
+        'Florida',
+        'California',
+        'Oregon'
+      ];
+    return lastName[Math.floor(Math.random() * 4)];
+  }
+  private generateId() {
+    const lastName =
+      [
+        234,
+        45,
+        97
+      ];
+    return lastName[Math.floor(Math.random() * 4)];
   }
 }
 
