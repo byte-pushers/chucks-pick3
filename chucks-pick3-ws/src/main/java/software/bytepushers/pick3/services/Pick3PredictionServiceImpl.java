1c4f06d8f19709e592fcadaa9b85b293c0f5440e
Aman Abrol <amanjotabroljava@gmail.com>
2020-08-20 22:16:24 -0700

    Trying to communicate the second lambda from ws

--- chucks-pick3-ws/src/main/java/software/bytepushers/pick3/services/Pick3PredictionServiceImpl.java
+++ chucks-pick3-ws/src/main/java/software/bytepushers/pick3/services/Pick3PredictionServiceImpl.java
@@ -1,5 +1,9 @@
 package software.bytepushers.pick3.services;
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.lambda.AWSLambda;
 import com.amazonaws.services.lambda.AWSLambdaClientBuilder;
 import com.amazonaws.services.lambda.invoke.LambdaInvokerFactory;
 import org.springframework.context.annotation.Profile;
@@ -12,8 +16,9 @@ public class Pick3PredictionServiceImpl implements Pick3PredictionService {
     @Override
     public int[][] generatePredictions(int winningNumber) {
         System.out.println("generatePredictions() method - start.");
+        AWSLambda awsLambda = AWSLambdaClientBuilder.standard().withRegion(Regions.US_EAST_2).build();
         final Pick3LottoSystemService pick3PredictionService = LambdaInvokerFactory.builder()
-                .lambdaClient(AWSLambdaClientBuilder.defaultClient())
+                .lambdaClient(awsLambda)
                 .build(Pick3LottoSystemService.class);
 
         System.out.println("generatePredictions() method - about to invoke lambda function.");
