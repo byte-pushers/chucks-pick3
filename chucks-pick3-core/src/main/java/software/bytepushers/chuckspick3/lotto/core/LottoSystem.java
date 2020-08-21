9fe6bfd29300401b9bbd7aa293a48abed24341a2
Aman Abrol <amanjotabroljava@gmail.com>
2020-08-20 17:14:24 -0700

    code for picks

--- chucks-pick3-core/src/main/java/software/bytepushers/chuckspick3/lotto/core/LottoSystem.java
+++ chucks-pick3-core/src/main/java/software/bytepushers/chuckspick3/lotto/core/LottoSystem.java
@@ -1,8 +1,6 @@
 package software.bytepushers.chuckspick3.lotto.core;
 
-import java.util.List;
 import java.util.Random;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class LottoSystem {
