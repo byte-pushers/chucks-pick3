d12f353fae1871d28218aca2acf7b6cbb1729eb1
ocampd1 <david.ocampo@bytepushers.software>
2020-11-12 11:38:03 -0600

    Fixed package json files.

--- chucks-pick3-web/angular-app/src/app/components/home.component/home.component.ts
+++ chucks-pick3-web/angular-app/src/app/components/home.component/home.component.ts
@@ -1,23 +1,56 @@
 import {Component, OnInit} from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
+
+
 
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [NgbCarouselConfig]
 })
 
 
 export class HomeComponent implements OnInit {
-  constructor() {
+
+  constructor(config: NgbCarouselConfig) {
+    config.wrap = true;
+    config.keyboard = false;
+    config.pauseOnHover = false;
   }
 
   ngOnInit() {
+
     const howToActive = document.getElementById('howTo');
     howToActive.classList.remove('active');
     howToActive.classList.add('allow-hover');
     const summaryActive = document.getElementById('summary');
     summaryActive.classList.remove('active');
     summaryActive.classList.add('allow-hover');
+  }
+
+  public isMobileResolution(): boolean {
+    let isMobileResolution: boolean = false;
+
+    if (window.innerWidth < 768) {
+      isMobileResolution = true;
+    } else {
+      isMobileResolution = false;
+    }
+
+    return isMobileResolution;
+  }
+
+  public isDesktopResolution(): boolean {
+    let isDesktopResolution: boolean = false;
+
+    if (window.innerWidth > 768) {
+      isDesktopResolution = true;
+    } else {
+      isDesktopResolution = false;
+    }
+
+    return isDesktopResolution;
   }
 }
