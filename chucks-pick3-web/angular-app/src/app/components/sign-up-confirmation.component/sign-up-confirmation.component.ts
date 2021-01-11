694157eb49ba8dd8b01f06aae3f20ef296f69a5f
ocampd1 <david.ocampo@bytepushers.software>
2021-01-11 15:10:29 -0600

    Completed work on page for confirmation.

--- chucks-pick3-web/angular-app/src/app/components/sign-up-confirmation.component/sign-up-confirmation.component.ts
+++ chucks-pick3-web/angular-app/src/app/components/sign-up-confirmation.component/sign-up-confirmation.component.ts
@@ -1,16 +1,25 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, OnDestroy} from '@angular/core';
 
 @Component({
   selector: 'app-sign-up-confirmation',
   templateUrl: './sign-up-confirmation.component.html',
   styleUrls: ['./sign-up-confirmation.component.css']
 })
-export class SignUpConfirmationComponent implements OnInit {
+export class SignUpConfirmationComponent implements OnInit, OnDestroy {
 
   constructor() {
   }
 
   ngOnInit() {
+    document.getElementById('sign-up-desktop').style.display = 'none';
+    document.getElementById('sign-up-tablet').style.display = 'none';
+    document.getElementById('sign-up-mobile').style.display = 'none';
+  }
+
+  ngOnDestroy() {
+    document.getElementById('sign-up-desktop').style.display = 'flex';
+    document.getElementById('sign-up-tablet').style.display = 'flex';
+    document.getElementById('sign-up-mobile').style.display = 'block';
   }
 
 }
