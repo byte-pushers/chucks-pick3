6a122b4e481237b984c084e3913e2380eb60672d
ocampd1 <ocampdesigns@gmail.com>
2021-03-03 14:32:48 -0800

    Completed implementation of popover.

---
+++ chucks-pick3-mobile/src/app/components/language-popover/language-popover.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-language-popover',
+  templateUrl: './language-popover.component.html',
+  styleUrls: ['./language-popover.component.scss'],
+})
+export class LanguagePopoverComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
--- chucks-pick3-mobile/src/app/home/home.page.ts
+++ chucks-pick3-mobile/src/app/home/home.page.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
-import { Pick3DrawDateCard } from "../models/pick3-draw-date-card";
-import { Pick3DrawDateCardDomain } from "../models/pick3-draw-date-card.domain";
-import {Pick3DrawTimeEnum} from "../models/pick3-draw-time.enum";
+import { Pick3DrawDateCard } from '../models/pick3-draw-date-card';
+import { Pick3DrawDateCardDomain } from '../models/pick3-draw-date-card.domain';
+import {Pick3DrawTimeEnum} from '../models/pick3-draw-time.enum';
+import {LanguagePopoverComponent} from '../components/language-popover/language-popover.component';
+import {PopoverController} from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -26,6 +28,14 @@ export class HomePage {
     new Pick3DrawDateCardDomain({}),
   ];
 
-  constructor() {}
-
+  constructor(private popoverCtrl: PopoverController) {}
+  async showPopover(ev: any) {
+    const popover = await this.popoverCtrl.create({
+      component: LanguagePopoverComponent,
+      cssClass: 'my-custom-class',
+      event: ev,
+      translucent: true
+    });
+    return await popover.present();
+  }
 }
