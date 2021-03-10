a6bc446429686b9153235ad433353345bfeff149
ocampd1 <ocampdesigns@gmail.com>
2021-03-10 14:39:55 -0800

    WIP Completed work on side menu now integrating with the rest of toolbar.

--- chucks-pick3-mobile/src/app/app.component.ts
+++ chucks-pick3-mobile/src/app/app.component.ts
@@ -1,28 +1,48 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import {Platform} from '@ionic/angular';
+import {SplashScreen} from '@ionic-native/splash-screen/ngx';
+import {StatusBar} from '@ionic-native/status-bar/ngx';
+import {title} from 'ionic/lib/color';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+    selector: 'app-root',
+    templateUrl: 'app.component.html',
+    styleUrls: ['app.component.scss']
 })
 export class AppComponent {
-  constructor(
-    private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar
-  ) {
-    this.initializeApp();
-  }
+    navigate: any;
+    constructor(
+        private platform: Platform,
+        private splashScreen: SplashScreen,
+        private statusBar: StatusBar
+    ) {
+        this.initializeApp();
+        this.sideMenu();
+    }
 
-  initializeApp() {
-    this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
+    initializeApp() {
+        this.platform.ready().then(() => {
+            this.statusBar.styleDefault();
+            this.splashScreen.hide();
 
-    });
-  }
+        });
+    }
+
+    sideMenu()
+    {
+        this.navigate =
+            [
+                {
+                    title : 'Account',
+                    url   : 'account',
+                    icon  : 'person'
+                },
+                {
+                    title : 'Settings',
+                    url   : 'settings',
+                    icon  : 'cog'
+                }
+            ];
+    }
 }
