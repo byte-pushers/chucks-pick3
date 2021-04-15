c6e180826b4b866d8d99bda8c4276d500a735ac0
Tonté Pouncil <pouncilt.developer@gmail.com>
2021-04-15 12:48:25 -0500

    initial commit.

--- chucks-pick3-mobile/karma.conf.js
+++ chucks-pick3-mobile/karma.conf.js
@@ -18,7 +18,47 @@ module.exports = function (config) {
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, '../coverage'),
       reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true
+      // Combines coverage information from multiple browsers into one report rather than outputting a report
+      // for each browser.
+      combineBrowserReports: true,
+      fixWebpackSourcePaths: true,
+      // Omit files with no statements, no functions and no branches covered from the report
+      skipFilesWithNoCoverage: true,
+
+      // Most reporters accept additional config options. You can pass these through the `report-config` option
+      'report-config': {
+        // all options available at: https://github.com/istanbuljs/istanbuljs/blob/73c25ce79f91010d1ff073aa6ff3fd01114f90db/packages/istanbul-reports/lib/html/index.js#L257-L261
+        html: {
+          // outputs the report in ./coverage/html
+          subdir: 'html'
+        }
+      },
+
+      // enforce percentage thresholds
+      // anything under these percentages will cause karma to fail with an exit code of 1 if not running in watch mode
+      thresholds: {
+        emitWarning: false, // set to `true` to not fail the test command when thresholds are not met
+        // thresholds for all files
+        global: {
+          statements: 100,
+          lines: 100,
+          branches: 100,
+          functions: 100
+        },
+        // thresholds per file
+        each: {
+          statements: 100,
+          lines: 100,
+          branches: 100,
+          functions: 100,
+          overrides: {
+            'baz/component/**/*.js': {
+              statements: 98
+            }
+          }
+        }
+      },
+      verbose: true
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
