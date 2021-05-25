60a6144ed43485ba0f09299c20a2c4943a524156
ocampd1 <ocampdesigns@gmail.com>
2021-05-25 13:40:35 -0700

    Changed minor detail in styling on state name.

---
+++ chucks-pick3-mobile/src/app/components/generate-picks-card/generate-picks-card.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-generate-picks-card',
+  templateUrl: './generate-picks-card.component.html',
+  styleUrls: ['./generate-picks-card.component.scss'],
+})
+export class GeneratePicksCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
