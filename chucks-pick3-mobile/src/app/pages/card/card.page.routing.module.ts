b72374ee0b98197bdeb1cc3af3aa097e0851fdbc
ocampd1 <ocampdesigns@gmail.com>
2021-06-10 15:54:56 -0500

    WIP trying to get generate-picks to displya.

--- chucks-pick3-mobile/src/app/pages/card/card.page.routing.module.ts
+++ chucks-pick3-mobile/src/app/pages/card/card.page.routing.module.ts
@@ -1,7 +1,7 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { CardPage } from './card.page';
-import {Pick3DrawDateInfoSection} from "../../components/pick3-draw-date-info-section/pick3-draw-date-info-section";
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {CardPage} from './card.page';
+import {Pick3DrawDateInfoSection} from '../../components/pick3-draw-date-info-section/pick3-draw-date-info-section';
 import {Pick3DrawTimeInfoSection} from '../../components/pick3-draw-time-info-section/pick3-draw-time-info-section';
 import {GeneratePicksCardComponent} from '../../components/generate-picks-card/generate-picks-card.component';
 
@@ -18,27 +18,17 @@ const routes: Routes = [
             },
             {
                 path: '',
-                outlet: 'secondary',
-                component: Pick3DrawTimeInfoSection
+                component: Pick3DrawTimeInfoSection,
+                outlet: 'secondary'
+            },
+            {
+                path: 'generate-picks',
+                component: GeneratePicksCardComponent,
+                outlet: 'secondary'
             }
         ]
     },
-    {
-        path: 'generate-picks',
-        children: [
-            {
-                path: '',
-                outlet: 'pick3-draw-date-info-section',
-                component: Pick3DrawDateInfoSection
-            },
-            {
-                path: '',
-                outlet: 'secondary',
-                component: GeneratePicksCardComponent
-            }
-        ]
-    }
-];
+    ];
 
 @NgModule({
     imports: [RouterModule.forChild(routes)],
