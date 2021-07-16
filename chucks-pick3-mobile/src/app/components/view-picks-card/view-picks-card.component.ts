a521653833330262765cdfd498d8e85fde2cd348
ocampd1 <ocampdesigns@gmail.com>
2021-07-16 13:56:38 -0500

    Completed work on bug, now shifting between times and displaying icons.

--- chucks-pick3-mobile/src/app/components/view-picks-card/view-picks-card.component.ts
+++ chucks-pick3-mobile/src/app/components/view-picks-card/view-picks-card.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {Pick3DrawTimeCard} from "../../models/pick3-draw-time-card";
+import {Pick3StateLottery} from "../../models/pick3-state-lottery";
+import {CardContextService} from "../../services/card-context.service";
+import {DrawTimeService} from "../../services/draw-time.service";
 
 @Component({
   selector: 'app-view-picks-card',
@@ -6,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-picks-card.component.scss'],
 })
 export class ViewPicksCardComponent implements OnInit {
+  public drawTimes: Array<Pick3DrawTimeCard> = [];
+  public pick3StateLottery: Pick3StateLottery;
+  private componentState;
 
-  constructor() { }
+  constructor(private cardContextService: CardContextService,
+              private drawTimeService: DrawTimeService) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.componentState = 'initializing';
+    this.cardContextService.context$.subscribe(context => {
+      this.drawTimes.splice(0, this.drawTimes.splice.length, ...context.drawTimes);
+      if (this.componentState === 'initializing') {
+        const currentDrawingTime = this.drawTimeService.getCurrentDrawTimeCard();
+        console.log(currentDrawingTime);
+        this.selectDrawingTimeCard(currentDrawingTime);
+      }
+    });
+    this.componentState = 'initialized';
+  }
 
+
+  public selectDrawingTimeCard(pick3DrawTimeCard: Pick3DrawTimeCard): void {
+    this.drawTimes.forEach(drawTime => {
+      if (drawTime.getDrawTime() !== pick3DrawTimeCard.getDrawTime()) {
+        drawTime.setSelected(false);
+      } else if (drawTime.getDrawTime() === pick3DrawTimeCard.getDrawTime()) {
+        drawTime.setSelected(true);
+        this.drawTimeService.setCurrentDrawTimeCard(drawTime);
+      }
+    });
+  }
 }
+
+
