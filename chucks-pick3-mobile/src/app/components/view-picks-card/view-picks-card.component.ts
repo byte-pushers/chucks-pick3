e5e71332af9192707a01bdb5d5cad8bea464f0fb
ocampd1 <ocampdesigns@gmail.com>
2021-10-12 10:26:36 -0700

    Compelted work for buttons and squashed.

--- chucks-pick3-mobile/src/app/components/view-picks-card/view-picks-card.component.ts
+++ chucks-pick3-mobile/src/app/components/view-picks-card/view-picks-card.component.ts
@@ -1,83 +1,63 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Pick3DrawTimeCard} from '../../models/pick3-draw-time-card';
 import {Pick3StateLottery} from '../../models/pick3-state-lottery';
 import {CardContextService} from '../../services/card-context.service';
 import {DrawTimeService} from '../../services/draw-time.service';
+import {IonicToastNotificationService} from '../../services/ionic-toast-notification.service';
+import {NumberUtilityService} from '../../services/numberUtility.service';
 
 @Component({
-  selector: 'app-view-picks-card',
-  templateUrl: './view-picks-card.component.html',
-  styleUrls: ['./view-picks-card.component.scss'],
+    selector: 'app-view-picks-card',
+    templateUrl: './view-picks-card.component.html',
+    styleUrls: ['./view-picks-card.component.scss'],
 })
 export class ViewPicksCardComponent implements OnInit {
-  public drawTimes: Array<Pick3DrawTimeCard> = [];
-  public pick3StateLottery: Pick3StateLottery;
-  private componentState;
-  public generatedNumbers: any;
-  generatedPicksArray: any[];
-  private savedArray: any[];
+    public drawTimes: Array<Pick3DrawTimeCard> = [];
+    public pick3StateLottery: Pick3StateLottery;
+    private componentState;
+    public generatedNumbers: any;
+    viewPicksArray: any[];
 
-
-  constructor(private cardContextService: CardContextService,
-              private drawTimeService: DrawTimeService) {
-  }
-
-  ngOnInit() {
-    this.retrieveNumbers();
-    this.componentState = 'initializing';
-    this.cardContextService.context$.subscribe(context => {
-      this.drawTimes.splice(0, this.drawTimes.splice.length, ...context.drawTimes);
-      if (this.componentState === 'initializing') {
-        const currentDrawingTime = this.drawTimeService.getCurrentDrawTimeCard();
-        this.selectDrawingTimeCard(currentDrawingTime);
-      }
-    });
-    this.componentState = 'initialized';
-  }
-
-
-  public selectDrawingTimeCard(pick3DrawTimeCard: Pick3DrawTimeCard): void {
-    this.drawTimes.forEach(drawTime => {
-      if (drawTime.getDrawTime() !== pick3DrawTimeCard.getDrawTime()) {
-        drawTime.setSelected(false);
-      } else if (drawTime.getDrawTime() === pick3DrawTimeCard.getDrawTime()) {
-        drawTime.setSelected(true);
-        this.drawTimeService.setCurrentDrawTimeCard(drawTime);
-      }
-    });
-  }
-
-  private retrieveNumbers() {
-    const generatedArray = this.drawTimeService.viewPicksArray;
-    const copiedArray = Object.assign([], generatedArray);
-    const ArrayToBeSetToTable: any = [];
-    for ( let i = 0; i < 12; i++){
-      ArrayToBeSetToTable.push(copiedArray.splice(Math.floor(Math.random() * copiedArray.length), 1));
+    constructor(private cardContextService: CardContextService,
+                private toastService: IonicToastNotificationService,
+                private drawTimeService: DrawTimeService,
+                private numbers: NumberUtilityService) {
     }
-    this.savedArray = Object.assign([], copiedArray);
-    this.createTable(ArrayToBeSetToTable);
-  }
-  private createTable(ArrayToBeSetToTable) {
-    const newArray = Object.assign([], ArrayToBeSetToTable);
-    const myTableDiv = document.getElementById('myDynamicTable');
-    const table = document.createElement('TABLE');
-    const tableBody = document.createElement('TBODY');
-    table.appendChild(tableBody);
 
-    for (let i = 0; i < 4; i++) {
-      const tr = document.createElement('ion-row');
-      tableBody.appendChild(tr);
+    ngOnInit() {
+        const numbersThatWereGenerated = this.drawTimeService.getCurrentDrawTimeCard().getPick3DrawTimeArray();
 
-      for (let j = 0; j < 3; j++) {
-        const generatedNumber = newArray.slice(0, 1);
-        const td = document.createElement('ion-col');
-        td.appendChild(document.createTextNode(generatedNumber));
-        tr.appendChild(td);
-        newArray.splice(0, 1);
-      }
+        if (numbersThatWereGenerated !== null && numbersThatWereGenerated !== undefined) {
+            this.viewPicksArray = numbersThatWereGenerated;
+            this.checkforSingleDigits(this.viewPicksArray);
+            console.log(this.drawTimeService.getCurrentDrawTimeCard());
+        } else {
+            this.toastService.presentToast('Results Not Available',
+                'Please generate numbers and come back later.', 'results-not-available');
+        }
+        this.componentState = 'initializing';
+        this.componentState = 'initialized';
     }
-    myTableDiv.appendChild(table);
-  }
 
 
+    public selectDrawingTimeCard(pick3DrawTimeCard: Pick3DrawTimeCard): void {
+        this.drawTimes.forEach(drawTime => {
+            if (drawTime.getDrawTime() !== pick3DrawTimeCard.getDrawTime()) {
+                drawTime.setSelected(false);
+            } else if (drawTime.getDrawTime() === pick3DrawTimeCard.getDrawTime()) {
+                drawTime.setSelected(true);
+                this.drawTimeService.setCurrentDrawTimeCard(drawTime);
+            }
+        });
+    }
+
+    private checkforSingleDigits(arrayTobeChecked) {
+        arrayTobeChecked.forEach(array => {
+            if (array.toString().length < 3) {
+                const index = arrayTobeChecked.indexOf(array);
+                const numberReplacement = this.numbers.padLeft(array, 3);
+                this.viewPicksArray.splice(index, 1, numberReplacement);
+            }
+        });
+    }
 }
