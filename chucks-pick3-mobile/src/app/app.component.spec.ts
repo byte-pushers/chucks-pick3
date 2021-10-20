cefdbe8922d7fa2a888333e521edf8cbc9a8019b
ocampd1 <ocampdesigns@gmail.com>
2021-10-20 15:44:32 -0700

    Completed tests for app.

--- chucks-pick3-mobile/src/app/app.component.spec.ts
+++ chucks-pick3-mobile/src/app/app.component.spec.ts
@@ -2,6 +2,8 @@ import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { TestBed, waitForAsync } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
+import {stringify} from 'querystring';
+import {EnumValue} from '@angular/compiler-cli/src/ngtsc/partial_evaluator';
 
 describe('AppComponent', () => {
 
@@ -18,6 +20,10 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
-  // TODO: add more tests!
+  it('#resetButtons() should pass an enum', function() {
+    const comp = AppComponent;
+    comp.resetButtons('gotoHome');
+    expect(comp.resetButtons).toBe(EnumValue, 'not an enum');
+  });
 
 });
