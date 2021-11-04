60a0aceb9d5eceaa004d6f0f7d79fc607b8d24d4
David Ocampo <46009248+ocampd1@users.noreply.github.com>
2021-11-04 11:26:44 -0700

    Completed tests for number Utility. (#558)
    * Completed tests for number Utility.
    
    * Made changes as requested.

--- chucks-pick3-mobile/src/app-vanilla/home-vanilla/home-vanilla.page.spec.ts
+++ chucks-pick3-mobile/src/app-vanilla/home-vanilla/home-vanilla.page.spec.ts
@@ -2,15 +2,17 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { HomeVanillaPage } from './home-vanilla.page';
+import {CardContextService} from '../../app/services/card-context.service';
 
-describe('HomePage', () => {
+describe('Vanilla Home Page Testing', () => {
   let component: HomeVanillaPage;
   let fixture: ComponentFixture<HomeVanillaPage>;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ HomeVanillaPage ],
-      imports: [IonicModule.forRoot()]
+      imports: [IonicModule.forRoot()],
+      providers: [CardContextService]
     }).compileComponents();
 
     fixture = TestBed.createComponent(HomeVanillaPage);
---
+++ chucks-pick3-mobile/src/app/pages/home/home.page.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+
+import { HomePage} from './home.page';
+
+describe('Home Page Testing', () => {
+  let component: HomePage;
+  let fixture: ComponentFixture<HomePage>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [ HomePage ],
+      imports: [IonicModule.forRoot()]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(HomePage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  xit('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
