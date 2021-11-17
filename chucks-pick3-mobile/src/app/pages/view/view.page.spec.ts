729c38ce6cfc7fe4ef3a979f9e3ccb5132fdf71d
ocampd1 <ocampdesigns@gmail.com>
2021-11-17 13:55:32 -0600

    WIP Working on adding work for tests.

--- chucks-pick3-mobile/src/app/pages/view/view.page.spec.ts
+++ chucks-pick3-mobile/src/app/pages/view/view.page.spec.ts
@@ -1,26 +1,25 @@
 import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
 import {IonicModule} from '@ionic/angular';
 
-import {HomePage} from './home.page';
+import {ViewPage} from './view.page';
 import {CommonModule} from '@angular/common';
-import {TranslateModule, TranslateService} from '@ngx-translate/core';
+import {TranslateModule} from '@ngx-translate/core';
 import {RouterTestingModule} from '@angular/router/testing';
 import {HttpClientTestingModule} from '@angular/common/http/testing';
 import {AppService} from '../../app.service';
 import {Pick3WebScrapingProviderService} from '../../providers/web-scraping/pick3-web-scraping-provider.service';
-import {CardContextService} from "../../services/card-context.service";
-import {Router} from "@angular/router";
-import {Pick3DrawTimeEnum} from "../../models/pick3-draw-time.enum";
+import {CardContextService} from '../../services/card-context.service';
+import {Router} from '@angular/router';
+import {Pick3DrawTimeEnum} from '../../models/pick3-draw-time.enum';
 
-describe('HomePage', () => {
-  let component: HomePage;
-  let fixture: ComponentFixture<HomePage>;
+describe('ViewPage', () => {
+  let component: ViewPage;
+  let fixture: ComponentFixture<ViewPage>;
   let router: Router;
-  let translateService: TranslateService;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [HomePage],
+      declarations: [ViewPage],
       imports: [CommonModule, IonicModule.forRoot(),
         TranslateModule.forRoot(),
         RouterTestingModule, TranslateModule, HttpClientTestingModule],
@@ -29,17 +28,12 @@ describe('HomePage', () => {
     router = TestBed.get(Router);
     spyOn(router, 'getCurrentNavigation').and.returnValue({ extras: { state: { currentSlideNumber: 7, currentDay: Pick3DrawTimeEnum.Pick3DrawTimeEnum.DAY } } } as any);
 
-    fixture = TestBed.createComponent(HomePage);
+    fixture = TestBed.createComponent(ViewPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
-  });
-
-  xit('should initiate initializePick3DrawDateCards', function () {
-    let spyOnLang = spyOn(translateService,'setDefaultLang');
-    expect(spyOnLang).toHaveBeenCalled();
   });
 });
