e33781da21cda9edb57ce2b34cda6bf559e49e16
ocampd1 <ocampdesigns@gmail.com>
2022-01-24 11:21:35 -0800

    Implemented swiper.

--- chucks-pick3-mobile/src/app/pages/home/home.page.module.ts
+++ chucks-pick3-mobile/src/app/pages/home/home.page.module.ts
@@ -1,41 +1,41 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { IonicModule } from '@ionic/angular';
-import { FormsModule } from '@angular/forms';
-import { HomePage } from './home.page';
-import { HomePageRoutingModule } from './home.page.routing.module';
-import { CardPageModule} from '../card/card.page.module';
-import { HttpClient } from '@angular/common/http';
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {IonicModule} from '@ionic/angular';
+import {FormsModule} from '@angular/forms';
+import {HomePage} from './home.page';
+import {HomePageRoutingModule} from './home.page.routing.module';
+import {CardPageModule} from '../card/card.page.module';
+import {HttpClient} from '@angular/common/http';
+import {TranslateModule, TranslateLoader} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import {SwiperModule} from "swiper/angular";
 
 export function createTranslateLoader(http: HttpClient) {
-    return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
 
 @NgModule({
-    imports: [
-        CommonModule,
-        FormsModule,
-        IonicModule,
-        TranslateModule.forChild({
-            loader: {
-                provide: TranslateLoader,
-                useFactory: createTranslateLoader,
-                deps: [HttpClient]
-            }
-        }),
-        HomePageRoutingModule,
-        CardPageModule
-    ],
-    exports: [
-
-    ],
-    declarations: [
-        HomePage
-    ]
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    SwiperModule,
+    TranslateModule.forChild({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: createTranslateLoader,
+        deps: [HttpClient]
+      }
+    }),
+    HomePageRoutingModule,
+    CardPageModule
+  ],
+  exports: [],
+  declarations: [
+    HomePage
+  ]
 })
 export class HomePageModule {
-    constructor() {
-    }
+  constructor() {
+  }
 }
