223325e58db3e214102f0588756a0dc142e46fa8
David Ocampo <46009248+ocampd1@users.noreply.github.com>
2022-04-01 15:57:53 -0700

    feat: completed work on timer (#629)
    Co-authored-by: Tonté Pouncil <tonte.pouncil@gmail.com>

--- chucks-pick3-mobile/src/app/components/countdown-timer/countdown-timer.component.spec.ts
+++ chucks-pick3-mobile/src/app/components/countdown-timer/countdown-timer.component.spec.ts
@@ -1,17 +1,34 @@
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
-import {IonicModule} from '@ionic/angular';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
 
-import {CountdownTimerComponent} from './countdown-timer.component';
+import { CountdownTimerComponent } from './countdown-timer.component';
+import { Router } from '@angular/router';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('CountdownTimerComponent', () => {
+  const date = new Date();
   let component: CountdownTimerComponent;
   let fixture: ComponentFixture<CountdownTimerComponent>;
-
+  let router: Router;
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [CountdownTimerComponent],
-      imports: [IonicModule.forRoot()]
+      imports: [
+        IonicModule.forRoot(),
+        RouterTestingModule,
+        HttpClientTestingModule,
+      ],
     }).compileComponents();
+    router = TestBed.get(Router);
+    spyOn(router, 'getCurrentNavigation').and.returnValue({
+      extras: {
+        state: {
+          currentSlideNumber: 7,
+          currentDay: date,
+        },
+      },
+    } as any);
 
     fixture = TestBed.createComponent(CountdownTimerComponent);
     component = fixture.componentInstance;
@@ -36,25 +53,26 @@ describe('CountdownTimerComponent', () => {
     expect(twoDigitTest).toEqual('06');
   });
 
-  it('should return a time difference',  () => {
+  it('should return a time difference', () => {
     const dateTime = 4;
     const timeDifferenceTest = component.getTimeDiff(dateTime, true);
     expect(timeDifferenceTest).toBeDefined();
   });
 
-  it('should return as "" ',  () => {
+  it('should return as "" ', () => {
     const today = new Date();
-    const time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
+    const time =
+      today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
     const timeDifferenceTest = component.getTimeDiff(time, true);
     expect(timeDifferenceTest).toEqual('');
   });
 
-  it('should return 00:00:00:00',  () => {
+  it('should return 00:00:00:00', () => {
     const timeDifferenceTest = component.getTimeDiff(0, false);
     expect(timeDifferenceTest).toEqual('00:00:00:00');
   });
 
-  it('should return 00:00:00:00',  () => {
+  it('should return 00:00:00:00', () => {
     const timeDifferenceTest = component.getTimeDiff(5, false);
     expect(timeDifferenceTest).toEqual('00:00:00:00');
   });
