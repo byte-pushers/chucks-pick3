5841b53b8dc2b4d5aa2f32c4ab478086473c5bb1
Tonte Pouncil <pouncilt.developer@gmail.com>
2022-05-18 16:46:11 -0500

    feat(638): got gulp task running; but still tring to only move certain directories

--- chucks-pick3-mobile/gulpfile.js
+++ chucks-pick3-mobile/gulpfile.js
@@ -1,15 +1,15 @@
 const { src, dest, task } = require("gulp"),
   clean = require("gulp-clean");
 
-var mockFilesToMove = ["test/mocks/**/*"];
+var mockFilesToMove = ["app/**/*"];
 
 task("remove-mocks", function () {
-  return src(["dist/*"], { read: false }).pipe(clean());
+  return src(["./src"], { read: false }).pipe(clean());
 });
 
 task("include-mocks", function (done) {
   // the base option sets the relative root for the set of files,
   // preserving the folder structure
-  src(mockFilesToMove, { base: "./", cwd: "src" }).pipe(dest("src"));
+  src(mockFilesToMove, { base: "./", cwd: "test/mocks" }).pipe(dest("src"));
   done();
 });
