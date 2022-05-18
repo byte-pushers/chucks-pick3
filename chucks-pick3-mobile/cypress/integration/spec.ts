a82c0d7d9e12038f63956926875b3ad6b1cf05c6
ocampd1 <ocampdesigns@gmail.com>
2022-05-17 17:26:43 -0700

    chore: completed work on nav

--- chucks-pick3-mobile/cypress/integration/spec.ts
+++ chucks-pick3-mobile/cypress/integration/spec.ts
@@ -51,10 +51,7 @@ describe('My First Test', () => {
   });
 
   it('should swipe left', () => {
-    cy.get('.swiper-slide-active')
-      .trigger('pointerdown', { which: 1 })
-      .trigger('pointermove', 'right')
-      .trigger('pointerup', { force: true });
+    cy.get('.swiper-slide-active').trigger('pointerdown', { which: 1 }).trigger('pointermove', 'right').trigger('pointerup', { force: true });
   });
 
   it('clicks on the drawTime button for day', () => {
@@ -70,5 +67,45 @@ describe('My First Test', () => {
 
     const stringDate = yesterday.toDateString();
     cy.contains(stringDate);
+  });
+
+  it('should swipe right', () => {
+    cy.get('.swiper-slide-active').trigger('pointerdown', { which: 1 }).trigger('pointermove', 'left').trigger('pointerup', { force: true });
+  });
+
+  it('should goto select-picks', function () {
+    cy.get('[id="generate-picks-icon"]').click({
+      multiple: true,
+      force: true,
+    });
+  });
+
+  /*
+  it('should change the draw-time to morning', function() {
+    cy.get('ion-select').click();
+  });
+*/
+
+  /*  it('should select a time of day', function() {
+  cy.get('ion-select-option').should('have.value', 'Morning').click();
+  });*/
+  it('should check if continue-button is false', function () {
+    cy.get('[id="continueButton"]').should('have.value', 'false');
+  });
+
+  it('should click on continue button to continue', function () {
+    cy.get('[id="continueButton"]').click({
+      multiple: true,
+      force: true,
+    });
+  });
+  it('should check if generate-button is false', function () {
+    cy.get('[id="generateButton"]').should('have.value', 'true');
+  });
+  it('should click on generate button to continue', function () {
+    cy.get('[id="generateButton"]').click({
+      multiple: true,
+      force: true,
+    });
   });
 });
