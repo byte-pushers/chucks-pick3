ee8ae5cc9b68eee948c1a99c48f82e86980a6649
Tonte Pouncil <pouncilt.developer@gmail.com>
2022-05-18 16:37:39 -0500

    feat(638): got gulp task running; but that is about it.

--- chucks-pick3-mobile/gulpfile.js
+++ chucks-pick3-mobile/gulpfile.js
@@ -1,14 +1,15 @@
 const { src, dest, task } = require("gulp"),
   clean = require("gulp-clean");
 
-var mockFilesToMove = ["./test/mocks/*.*"];
+var mockFilesToMove = ["test/mocks/**/*"];
 
-gulp.task("remove-mocks", function () {
+task("remove-mocks", function () {
   return src(["dist/*"], { read: false }).pipe(clean());
 });
 
-gulp.task("include-mocks", function () {
+task("include-mocks", function (done) {
   // the base option sets the relative root for the set of files,
   // preserving the folder structure
   src(mockFilesToMove, { base: "./", cwd: "src" }).pipe(dest("src"));
+  done();
 });
