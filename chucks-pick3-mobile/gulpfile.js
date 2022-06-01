c2a682711cba39594a387005e49d4c0790e8363d
ocampd1 <ocampdesigns@gmail.com>
2022-06-01 11:20:14 -0500

    chore: completed work on intial gulp cmd

--- chucks-pick3-mobile/gulpfile.js
+++ chucks-pick3-mobile/gulpfile.js
@@ -1,6 +1,19 @@
 const { src, dest, task } = require("gulp"),
   clean = require("gulp-clean"),
   gutil = require("gulp-util");
+const exec = require("child_process").exec;
+
+exports.default = defaultTask;
+
+function defaultTask(cb) {
+  // place code for your default task here
+  if (process.env.USE_MOCKS === "true") {
+    exec("ionic serve --configuration=mock");
+  } else {
+    exec("npm run use-prod && ionic serve --configuration=prod");
+  }
+  cb();
+}
 
 task("remove-mocks", function () {
   return src(["src/**/*mock*.*"], { read: false }).pipe(clean());
@@ -9,16 +22,11 @@ task("remove-mocks", function () {
 task("include-mocks", function (done) {
   // the base option sets the relative root for the set of files,
   // preserving the folder structure
-  console.log("inside inlcude-mocks gulp tasks.");
   if (process.env.USE_MOCKS === "true") {
     src(["test/mocks/**/*.*", "test/environments/**/*.*"]).pipe(dest("src"));
   }
 
   done();
-});
-
-task("remove-prod", function () {
-  return src(["src/**/*prod*.*"], { read: false }).pipe(clean());
 });
 task("include-prod", function (done) {
   // the base option sets the relative root for the set of files,
