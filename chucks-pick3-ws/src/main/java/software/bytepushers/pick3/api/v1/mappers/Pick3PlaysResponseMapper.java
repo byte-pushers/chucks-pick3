a2e85adefc5b082ef120857a799e134540e6009d
Tonté Pouncil <tonte.pouncil.cs@bcbsa.com>
2023-01-28 07:40:37 -0600

    chore(cp3-683): fix one more failing test; but still have one outstanding failure

--- chucks-pick3-ws/src/main/java/software/bytepushers/pick3/api/v1/mappers/Pick3PlaysResponseMapper.java
+++ chucks-pick3-ws/src/main/java/software/bytepushers/pick3/api/v1/mappers/Pick3PlaysResponseMapper.java
@@ -10,14 +10,14 @@ import java.text.SimpleDateFormat;
 
 @Component
 public class Pick3PlaysResponseMapper implements Pick3PlaysMapper {
-    private DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+    // private DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
     @Override
     public Pick3PlaysResponse pick3PlaysToPick3PlaysResponse(Pick3Plays pick3Plays) {
         Pick3PlaysResponse response = null;
 
         if (pick3Plays != null) {
             response = new Pick3PlaysResponse();
-            if (pick3Plays.getDrawingDate() != null) response.setDate(this.df.format(pick3Plays.getDrawingDate()));
+            if (pick3Plays.getDrawingDate() != null) response.setDate(pick3Plays.getDrawingDate().toString());
             if (pick3Plays.getDrawingTime() != null) response.setDrawingTime(pick3Plays.getDrawingTime().name());
             response.setPlays(pick3Plays.getPlays());
         }
